8e54a64c10e24e079f800b24dbed3f85f3d621d8
elrilacho <camilobr17@gmail.com>
2023-06-20 08:45:58 -0500

    tema4_listo

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,8 +5,180 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 
-
+      h1 ISO 9000 Organización Internacional de Normalización
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-3.col-8.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema4/1.svg', alt='')
+      .col-lg-9: p.mb-0 A través de los tiempos cuando dos o varias compañías u organizaciones tenían la intención de hacer negocios, se analizaban cada una, las que fungían de clientes y las que a su vez jugaban el rol de proveedores, también hacían parte de esta ecuación cuando se trataba de hacer alianzas o sociedades. Se solicitaba información de todas las partes sobre aspectos que son importantes en una empresa ideal, como se puede ver a continuación:
+    h4.text-center.mb-5: span.color-acento-contenido.fw-bold Aspectos importantes en una empresa ideal
+    .row.justify-content-center.mb-5
+      .col-xl-3.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema4/2.svg", alt="alt")
+          .crd_hover_txt--body(style="background-color: #FFE6EA")
+            h4.mb-3(style="color: #12263F") Características de sus empleados 
+            ul.lista-ul--color.text-start
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Procesos de convocatoria, selección y contratación.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Formación y competencias.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Conocimiento de los procesos y los equipos.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Programas de mejoramiento y reconocimientos
+      .col-xl-3.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema4/3.svg", alt="alt")
+          .crd_hover_txt--body(style="background-color: #FFE6EA")
+            h4.mb-3(style="color: #12263F") Información sobre sus proveedores 
+            ul.lista-ul--color.text-start
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Quiénes son.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Trayectoria y reconocimiento en el sector.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Calificaciones y certificaciones.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Cumplimiento y portafolio de servicios
+      .col-xl-3.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema4/4.svg", alt="alt")
+          .crd_hover_txt--body(style="background-color: #FFE6EA")
+            h4.mb-3(style="color: #12263F") Información sobre las máquinas y equipos
+            ul.lista-ul--color.text-start
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Obsolescencia.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Tecnología actualizada.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Información pertinente sobre máquinas y herramientas
+      .col-xl-3.col-lg-7.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema4/5.svg", alt="alt")
+          .crd_hover_txt--body(style="background-color: #FFE6EA")
+            h4.mb-3(style="color: #12263F") Información sobre sus procesos
+            ul.lista-ul--color.text-start
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Tecnología utilizada.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Eficiencias y controles.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Capacidad instalada.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Sistemas de producción.
+              li
+                i.fas.fa-tshirt(style="color: #12263F") 
+                | Sistemas de Información
+    p.mb-5(data-aos='fade-right') Los demás aspectos importantes que se consideran al iniciar negocios entre dos compañías requieren un proceso que puede tomar tiempo, dependiendo del tamaño del negocio, la naturaleza de las compañías y la duración prevista de la relación. Esta evaluación minuciosa se realiza para determinar si la relación comercial es viable o no, y puede llevar días, semanas, meses e incluso años. Anteriormente, esta actividad era costosa y a menudo generaba obstáculos en el desarrollo normal de la producción y la comercialización a nivel mundial.
+    .row.justify-content-center.mb-5.g-0
+      .col-lg-4: img(src='@/assets/curso/temas/tema3/13.jpg', alt='')
+      .col-lg-8
+        .p-5(style="background-color: #CEFAFB").h-100.d-flex.align-items-center
+          p.mb-0 En 1946, representantes de 25 países se reunieron en Londres debido a preocupaciones comunes sobre la demora y el costo de resolver problemas relacionados con la calidad. Un año después, en 1947, la Organización Internacional de Normalización (ISO) comenzó a funcionar, con la principal tarea de aprobar los procesos y procedimientos de las empresas, certificarlas y, a través de este certificado, permitir que las empresas omitieran la mayoría de los procesos de selección de clientes y proveedores.
+    p.mb-5(data-aos='fade-right') En resumen, la ISO se encarga de evaluar a las compañías que solicitan su certificación, y aprueba o rechaza la excelencia de sus procesos en base a un conjunto de normas desarrolladas en todo el mundo que se adaptan a todas las compañías y países, normalizando o estandarizando los procesos.
+    .row.justify-content-center.mb-5
+      .col-lg-8.mb-lg-0.mb-3
+        .cajon.color-acento-botones.p-5.mb-4
+          p.mb-0 La ISO es una organización independiente, no es gubernamental y es la mayor creadora de normas reconocidas internacionalmente y busca homologar procesos y especificaciones, con el fin de ayudar a la dinámica global de los negocios, que se vive en el mundo. Adicional a construir normas, esta organización también insta a que las empresas implementen en sus negocios programas de mejoramiento continuo a todo nivel. En la actualidad hay más de 22.000 normas que se relacionan con sectores como: producción, industria, salud, alimentos, construcción, etc. Su sede está en Ginebra Suiza, y a la fecha cuenta con 164 países miembros.
+        p Para obtener un certificado de parte de la ISO que acredite que los procesos están normalizados, entendidos, ejecutados y documentados; #[span.color-acento-contenido.fw-bold es necesario tener en cuenta los siguientes requerimientos, a continuación se pueden ver:]
+      .col-lg-4.col-8: img(src='@/assets/curso/temas/tema4/7.png', alt='')
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+      .tarjeta(style="background-color: #FFA884 ").p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/curso/temas/tema4/8.svg' alt='AvatarTop')
+        p.text-center Saber a cuál norma deseo aplicar para ser certificado.
+      .tarjeta(style="background-color: #FFA884 ").p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/curso/temas/tema4/9.svg' alt='AvatarTop')
+        p.text-center Hacer un diagnóstico de la compañía para identificar áreas susceptibles de mejoramiento. 
+      .tarjeta(style="background-color: #FFA884 ").p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/curso/temas/tema4/10.svg' alt='AvatarTop')
+        p.text-center Si no se tiene un Sistema de Gestión de Calidad, construir e implementar uno.
+      .tarjeta(style="background-color: #FFA884 ").p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/curso/temas/tema4/11.svg' alt='AvatarTop')
+        p.text-center Documentar todos los procesos que se realizan y llevar registros que evidencien que esos procesos se realizan de acuerdo con el documento.
+      .tarjeta(style="background-color: #FFA884 ").p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/curso/temas/tema4/12.svg' alt='AvatarTop')
+        p.text-center Diseñar e implementar un programa de capacitación constante.
+      .tarjeta(style="background-color: #FFA884 ").p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/curso/temas/tema4/13.svg' alt='AvatarTop')
+        p.text-center Si ya realiza auditorias, mejorarlas y si no, comenzarlas a hacer.
+    p Las organizaciones para poder administrar y mejorar sus procesos, se han valido de las Normas Técnicas de Calidad (NTC), para que le ayuden a establecer directrices de desarrollo y funcionamiento de sus SGC.
+    .cajon.color-acento-contenido.p-4.mb-4
+      p.mb-0 Según la Universidad Continental, (2020) la Norma ISO 9000, fija los principios fundamentales de gestión de calidad Que ayudan a las organizaciones a controlar y mejorar su rendimiento y conducirlos hacia la eficiencia, la excelencia de sus productos y la optimización de su servicio al cliente. Estos principios son: enfoque al cliente, liderazgo, participación del personal, enfoque basado en los procesos, enfoque para la gestión, mejora continua, enfoque basado en la toma de decisiones y las buenas relaciones con el proveedor.
+    .row.justify-content-center.mb-5
+      .col-lg-8.mb-lg-0.mb-3
+        p Además, la obtención de la certificación ISO 9000, trae consigo una serie de ventajas para la empresa, pero ¿cuáles son?, #[span.color-acento-contenido.fw-bold a continuación se pueden ver:]
+        h4.text-center: span.color-acento-contenido Ventajas de la ISO 9000
+        TabsA.color-acento-botones.mb-5
+          .tarjeta.p-4(titulo="Mejora" style="background-color: #F5E0E4 ")
+            h4 Mejora
+            p Permite un enfoque a la mejora continua.
+            .d-flex.justify-content-end
+              img(src='@/assets/curso/temas/tema4/14.svg', alt='' style="width: 77px")
+          .tarjeta.p-4(titulo="Visibilidad" style="background-color: #F5E0E4 ")
+            h4 Visibilidad
+            p El reconocimiento con la certificación hace a la empresa más visible con respecto a la imagen que proyecta en el mercado y en su competencia. Genera más credibilidad, frente a quienes no han sido certificados.
+            .d-flex.justify-content-end
+              img(src='@/assets/curso/temas/tema4/15.svg', alt='' style="width: 77px")
+          .tarjeta.p-4(titulo="Comunicación" style="background-color: #F5E0E4 ")
+            h4 Comunicación
+            p Todas las actividades que se realizan al interior de las organizaciones, deben estar siempre bien relacionadas e integradas, la certificación obliga a eso, y la comunicación entre áreas se mejora.
+            .d-flex.justify-content-end
+              img(src='@/assets/curso/temas/tema4/16.svg', alt='' style="width: 77px")
+          .tarjeta.p-4(titulo="Participación" style="background-color: #F5E0E4 ")
+            h4 Participación
+            p Obtener una certificación ISO 9000, mejora de manera importante la posibilidad de participar en mercados internacionales.
+            .d-flex.justify-content-end
+              img(src='@/assets/curso/temas/tema4/17.svg', alt='' style="width: 77px")
+          .tarjeta.p-4(titulo="Relacionamiento" style="background-color: #F5E0E4 ")
+            h4 Relacionamiento
+            p Las negociaciones se vuelven más ágiles y oportunas cuando se tiene un certificado ISO, la capacidad de relacionamiento crece.
+            .d-flex.justify-content-end
+              img(src='@/assets/curso/temas/tema4/18.svg', alt='' style="width: 77px")
+          .tarjeta.p-4(titulo="Confianza" style="background-color: #F5E0E4 ")
+            h4 Confianza 
+            p La certificación en NTC, requiere la realización de ejercicios sobre todos los procesos organizacionales, buscando la excelencia, y en pro del beneficio del cliente. Obtener la satisfacción del cliente es un buen logro, y al ser certificados él tendrá más confianza en la organización.
+            .d-flex.justify-content-end
+              img(src='@/assets/curso/temas/tema4/19.svg', alt='' style="width: 77px")
+      .col-lg-4.col-8: img(src='@/assets/curso/temas/tema4/20.png', alt='')
+    .p-4(style="background-color: #2F3C4B ")
+      .row.justify-content-center.align-items-center
+        .col-lg-2.col-8.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema4/21.svg', alt='')
+        .col-lg-10
+          p.mb-0.text-white Para una visión más amplia sobre las NTC ISO 9000, por favor consulte  en el material complementario el video denominado: Anatomía de las normas ISO.
 </template>
 
 <script>
